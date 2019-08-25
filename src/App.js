8198834deed3038a1e66f454f7790c6a2fdaca83
pkumar08 <pankaj.kumar1@decathlon.com>
2019-08-25 21:42:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Pankaj!! Please edit <code>src/App.js</code> and save to reload to see some awesome magic.
         </p>
         <a
           className="App-link"
